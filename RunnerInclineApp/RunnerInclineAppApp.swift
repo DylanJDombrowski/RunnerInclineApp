749a0194a31afea62c95011b895db96b1869a289
DylanJDombrowski <dyland601@gmail.com>
2025-10-18 22:50:28 -0400

    Initial Commit

---
+++ RunnerInclineApp/RunnerInclineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnerInclineAppApp.swift
+//  RunnerInclineApp
+//
+//  Created by Dylan Dombrowski on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct RunnerInclineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
